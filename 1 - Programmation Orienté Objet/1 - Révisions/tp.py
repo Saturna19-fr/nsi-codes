ef3f5d7e1c5359bfe3f0d5962de5efa657247403
Florian Picard <florian.picard.fr@gmail.com>
2024-09-04 18:06:09 +0200

    premier tp

---
+++ 1 - Programmation Orienté Objet/1 - Révisions/tp.py
@@ -0,0 +1,43 @@
+def est_pair(bits):
+    """ [int] -> bool
+    len(bits) > 0
+    Détermine si le nombre dont l'écriture en base 2 est donnée par le tableau bits est pair.  """
+    pass
+
+def est_plus_grand_kbits(bits):
+    """ [int] -> bool
+    k = len(bits) > 0
+    Détermine si le tableau bits correspond au plus  grand entier que l'on  puisse écrire sur k bits.  """
+    pass
+
+def base2_vers_base10(bits):
+    """ [int] -> int
+    Détermine le nombre entier dont l'écriture en base 2 est donnée par le tableau bits """
+    pass
+
+def appartient(tab, e):
+    """ [int], int -> bool
+    Détermine si l'élément e est présent dans le tableau tab.  """
+    pass
+
+def indices_occurrences(tab, e):
+    """ [int], int -> [int]
+    Détermine les indices des occurrences de e dans le tableau tab.  """
+    pass
+
+def occurrences(tab):
+    """ [int] -> { int:[int] }
+    Associe à chaque valeur de tab la liste des indices des occurrences de cette valeur """
+    pass
+
+def maximum(tab):
+    """ [int], int -> int, int
+    len(tab) > 0
+    Détermine le maximum des éléments de tab, ainsi que le premier indice pour lequel ce maximum est atteint.  """
+    pass
+
+def est_trie_croissant(tab):
+    """ [int] -> bool
+    Détermine si le tab est trié par ordre croissant """
+    pass
+
